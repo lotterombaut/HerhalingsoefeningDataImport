2063f2da19e405ff2d0a41d89465cc4a9f44c64b
Lotte <lotte.rombaut@student.howest.be>
2019-12-10 08:41:53 +0100

    class1

---
+++ Helloworldd/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helloworldd
+{
+    public class Class1
+    {
+        public static string HalloWorld()
+        {
+            return "hello world";
+        }
+    }
+}
